3652625d142e142023c894120993e4ab66214170
Shalom <ShalomTaiwo@my.uopeople.edu>
2021-04-17 00:06:49 +0200

    contact form

--- contact_form/contact_form.php
+++ contact_form/contact_form.php
@@ -1,7 +1,7 @@
 <?php
 
 // configure
-$from = 'info@comfortadeola.cf'; // Replace it with Your Hosting Admin email. REQUIRED!
+$from = 'tshalom01@gmail.com'; // Replace it with Your Hosting Admin email. REQUIRED!
 $sendTo = 'comfirst01@gmail.com'; // Replace it with Your email. REQUIRED!
 $subject = 'New message from contact form';
 $fields = array('name' => 'Name', 'email' => 'Email', 'subject' => 'Subject', 'message' => 'Message'); // array variable name => Text to appear in the email. If you added or deleted a field in the contact form, edit this array.
